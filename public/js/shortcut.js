0991c9ed0d0b50083a900b4a4d00ec8d43e88318
richard <yueyericardo@gmail.com>
2019-10-21 00:21:06 -0400

    update shortcut

--- public/js/shortcut.js
+++ public/js/shortcut.js
@@ -1,12 +1,12 @@
 document.onkeyup = function(e) {
   if (e.ctrlKey && e.which == 78) {
     // alert("Ctrl + N shortcut combination was pressed");
-    window.location.href = "note/create";
+    window.location.href = "/note/create";
   } else if (e.altKey && e.which == 78) {
     // alert("Alt + N shortcut combination was pressed");
-    window.open("note/create");
+    window.open("/note/create");
   } else if (e.ctrlKey && e.which == 72) {
     // alert("Ctrl + H shortcut combination was pressed");
-    window.location.href = "notes";
+    window.location.href = "/notes";
   }
 };
